bac69cc486ac5a5b8b2442373acf150c260bd540
Jin Xu <aaron.jin.xu@gmail.com>
2019-06-18 14:15:20 +0100

    sync local code

--- model.py
+++ model.py
@@ -116,7 +116,7 @@ class LEO(snt.AbstractModule):
     val_loss, val_accuracy = self.calculate_inner_loss(
         data.val_input, data.val_output, adapted_classifier_weights)
 
-    # val_loss += self._kl_weight * kl
+    val_loss += self._kl_weight * kl
     # val_loss += self._encoder_penalty_weight * encoder_penalty
 
 
